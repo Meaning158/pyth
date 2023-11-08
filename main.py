a76136258af45830a121f6bc277c32047cb023aa
Meaning158 <127957279+Meaning158@users.noreply.github.com>
2023-11-08 17:59:40 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plot
+t = np.arange(0, np.pi * 2, 0.1);
+a = np.sin(t)
+plot.plot(t, a)
+plot.grid(True, which='both')
+plot.axhline(y=0, color='k')
+plot.show()
